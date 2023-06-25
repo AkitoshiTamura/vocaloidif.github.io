2362475da44ed1cbcde64e6780da1e557fd7bf38
AkitoshiTamura <137361724+AkitoshiTamura@users.noreply.github.com>
2023-06-25 16:39:53 +0900

    Create openbtn.js

---
+++ openbtn.js
@@ -0,0 +1,58 @@
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+      $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+  });
+  
+  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+      $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+      $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+      $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+  });
+
+//ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
+
+function fadeAnime(){
+    // flipLeft
+    $('.gallery li').each(function(){ 
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight){
+            $(this).addClass('flipLeft');
+        }else{
+            $(this).removeClass('flipLeft');
+        }
+    });
+    }
+    
+    // 画面をスクロールをしたら動かしたい場合の記述
+      $(window).scroll(function (){
+        fadeAnime();/* アニメーション用の関数を呼ぶ*/
+      });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+      $('.slider').slick({
+        autoplay: true,//自動的に動き出すか。初期値はfalse。
+        infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+        slidesToShow: 3,//スライドを画面に3枚見せる
+        slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+        prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+        nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+        dots: true,//下部ドットナビゲーションの表示
+        responsive: [
+          {
+          breakpoint: 769,//モニターの横幅が769px以下の見せ方
+          settings: {
+            slidesToShow: 2,//スライドを画面に2枚見せる
+            slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+          }
+        },
+        {
+          breakpoint: 426,//モニターの横幅が426px以下の見せ方
+          settings: {
+            slidesToShow: 1,//スライドを画面に1枚見せる
+            slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+          }
+        }
+      ]
+      });
